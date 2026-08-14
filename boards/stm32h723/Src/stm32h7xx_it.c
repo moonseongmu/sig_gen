c53f978e1d6b59a82ca7bcf35b1c8cea062e1d1e
Moon Seongmu <76660830+moonseongmu@users.noreply.github.com>
2026-08-14 23:40:23 +0800

    removed main.h include

--- boards/stm32h723/Src/stm32h7xx_it.c
+++ boards/stm32h723/Src/stm32h7xx_it.c
@@ -18,7 +18,6 @@
 /* USER CODE END Header */
 
 /* Includes ------------------------------------------------------------------*/
-#include "main.h"
 #include "stm32h7xx_it.h"
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
